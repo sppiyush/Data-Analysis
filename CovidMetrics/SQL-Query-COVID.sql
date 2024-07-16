16d0d3c5042fa4ce3a20cbf6ab8b163effe096c5
Piyush Kumar <piyushpkcorp@gmail.com>
2024-07-16 11:35:17 +0530

    Update SQL-Query-COVID.sql

--- CovidMetrics/SQL-Query-COVID.sql
+++ CovidMetrics/SQL-Query-COVID.sql
@@ -17,7 +17,7 @@ Select Location, date, total_cases, new_cases, total_deaths, population
 From CovidMetrics..CovidDeaths
 where continent is not null 
 order by 1,2
-
+-------------------------------------------------------------------------------------------------------------------------------------------
 -- Looking at Total Cases vs Total Deaths 
 -- Shows likelihood of dying if you contract covid in India and Pakistan 
 Select Location,date,total_cases,total_deaths,(total_deaths/total_cases)*100 AS DeathPercentage
@@ -25,7 +25,7 @@ FROM CovidMetrics..CovidDeaths
 WHERE Location like '%India%' OR Location like '%Pakistan%'  -- In order to get the values of both the countries we need to use OR statement 
 AND continent is not null 
 order by 1,2
-
+--------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Looking at Total Cases vs Population
 -- Shows what percentage of population got Covid in India and Pakistan 
@@ -34,20 +34,21 @@ From CovidMetrics..CovidDeaths
 WHERE Location like '%India%' OR Location like '%Pakistan%'  --Like statement is case insensitive 
 AND continent is not null
 Order by 1,2
-
+----------------------------------------------------------------------------------------------------------------------------------------------
 -- Looking at Countries with Highest Infection Rate compared to Population
 Select Location, Population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases / Population))*100 AS PercentPopulationInfected
 FROM CovidMetrics..CovidDeaths 
 WHERE continent is not null 
 GROUP BY Location,Population 
 ORDER BY PercentPopulationInfected DESC
-
+-----------------------------------------------------------------------------------------------------------------------------------------------
 -- Showing Countries with the Highest Death Count per Population
 Select Location, MAX(cast(Total_deaths as int)) AS TotalDeathCount --since the datatype used in total_deaths is not giving the value you are looking for so we will probably cast it 
 FROM CovidMetrics..CovidDeaths 
 WHERE continent is not null 
 GROUP BY Location 
 ORDER BY TotalDeathCount DESC
+-----------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Time to break things down by continent 
 Select continent, MAX(cast(Total_deaths as int)) AS TotalDeathCount
@@ -55,13 +56,14 @@ FROM CovidMetrics..CovidDeaths
 WHERE continent is not null
 GROUP BY continent  --It looks like it is giving more data from USA and Canada only for North America which is not that accurate 
 ORDER BY TotalDeathCount DESC 
-
+-------------------------------------------------------------------------------------------------------------------------------------------------
 -- Following query looks more accurate as compared to the above one 
 Select Location, MAX(cast(Total_deaths as int)) AS TotalDeathCount
 FROM CovidMetrics..CovidDeaths
 WHERE continent is null
 GROUP BY Location
 ORDER BY TotalDeathCount DESC 
+--------------------------------------------------------------------------------------------------------------------------------------------------
 
 --GLOBAL NUMBERS 
 Select date, SUM(new_cases) AS TotalNewCases, SUM(cast(new_deaths as int)) AS TotalNewDeaths , SUM(cast(new_deaths as int))/SUM(cast(new_cases as int))*100 as DeathPercentage 
@@ -70,7 +72,7 @@ FROM CovidMetrics..CovidDeaths
 WHERE continent is not null --If not then 'Divide by zero error will get encountered'
 GROUP BY date
 ORDER BY 1,2
-
+--------------------------------------------------------------------------------------------------------------------------------------------------
 --Looking at Total Population vs Vaccinations in India 
 --We will be joining both the tables 
 
@@ -92,7 +94,7 @@ Join CovidMetrics..CovidVaccinations vac
 	and dea.date = vac.date 
 where dea.continent is not null 
 ORDER BY 2,3
-
+----------------------------------------------------------------------------------------------------------------------------------------------------
 --USE CTEs
 With PopvsVac (Continent, Location, Date, Population,New_Vaccinations, RollingPeopleVaccinated)
 AS
@@ -110,7 +112,7 @@ where dea.continent is not null
 
 Select * , (RollingPeopleVaccinated/Population)*100
 From PopvsVac
-
+----------------------------------------------------------------------------------------------------------------------------------------------------
 -- Using Temp Table to perform Calculation on Partition By in previous query
 DROP Table if exists #PercentPopulationVaccinated
 Create Table #PercentPopulationVaccinated
@@ -138,7 +140,7 @@ Select *, (RollingPeopleVaccinated/Population)*100
 From #PercentPopulationVaccinated
 
 
-
+-----------------------------------------------------------------------------------------------------------------------------------------------
 -- Creating View to store data for later visualizations
 
 Create View PercentPopulationVaccinated as
